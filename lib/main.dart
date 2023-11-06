01b570aebacbf61972cf8810029faea6b5954c70
kalanyanee Amonvachagun <Amonvachagun_k@silpakorn.edu>
2023-11-06 19:55:21 +0700

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:project/scressns.home/home_page.dart';
 
 void main() {
-  runApp(WelcomeApp());
+  runApp(MyApp());
 }
 
-class WelcomeApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomeScreen(),
-    );
-  }
-}
-
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.lightGreen[200],
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: AppBar(
-          backgroundColor: Colors.green,
-          title: Container(
-            margin: EdgeInsets.all(20),
-            child: Text(
-              'วันนี้คุณเป็นอย่างไรบ้าง?',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          centerTitle: true,
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // กรอบพื้นหลังสีแดง
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(20), // กำหนดความโค้ง
-                  ),
-                ),
-                SizedBox(width: 10), // ระยะห่างระหว่างกรอบ
-                // กรอบพื้นหลังสีเขียว
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(20), // กำหนดความโค้ง
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10), // ระยะห่างระหว่างแถวขอบตัวแรกและตัวที่ 2
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // กรอบพื้นหลังสีฟ้า
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20), // กำหนดความโค้ง
-                  ),
-                ),
-                SizedBox(width: 10), // ระยะห่างระหว่างกรอบ
-                // กรอบพื้นหลังสีเหลือง
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(20), // กำหนดความโค้ง
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      home: HomePage(), // เรียกใช้ HomePage จากไฟล์ home_page.dart
     );
   }
 }
---
+++ lib/scressns.home/home_page.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const double borderRadius = 10.0; // ความโค้งขอบ
+    const double borderWidth = 1.0; // ความหนาขอบ
+    const double shadowBlurRadius = 5.0; // รัศมีความเบลอของเงา
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          backgroundColor: Colors.green[300],
+          title: Container(
+            margin: EdgeInsets.all(20),
+            child: Text(
+              'ยินดีต้อนรับ',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topCenter, // จัดวางด้านบน
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.circular(borderRadius), // ความโค้งขอบ
+                      border: Border.all(
+                        color: Colors.green.shade300, // สีเขียว
+                        width: borderWidth, // ความหนาขอบ
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color:
+                              Colors.green.withOpacity(0.5), // สีเขียวที่เบลอ
+                          blurRadius: shadowBlurRadius, // รัศมีความเบลอ
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 240,
+                          height: 270,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(borderRadius),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Image.asset(
+                            'assets/images/air.png',
+                            fit: BoxFit.contain, // ปรับขนาดให้พอดีกับ Container
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          left: 0,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'ทำความสะอาดเครื่องปรับอากาศ',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                      border: Border.all(
+                        color: Colors.green.shade300,
+                        width: borderWidth,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.green.withOpacity(0.5),
+                          blurRadius: shadowBlurRadius,
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 240,
+                          height: 270,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(borderRadius),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Image.asset(
+                            'assets/images/mop.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          left: 0,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'ทำความสะอาดบ้าน',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.topCenter, // จัดวางด้านบน
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                      border: Border.all(
+                        color: Colors.green.shade300,
+                        width: borderWidth,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.green.withOpacity(0.5),
+                          blurRadius: shadowBlurRadius,
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 240,
+                          height: 270,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(borderRadius),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Image.asset(
+                            'assets/images/home.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          left: 0,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'Deep Cleaning',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(borderRadius),
+                      border: Border.all(
+                        color: Colors.green.shade300,
+                        width: borderWidth,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.green.withOpacity(0.5),
+                          blurRadius: shadowBlurRadius,
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 240,
+                          height: 270,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(borderRadius),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: Image.asset(
+                            'assets/images/calendar.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 10,
+                          left: 0,
+                          right: 0,
+                          child: Center(
+                            child: Text(
+                              'จองงานแบบแพ็คเกจ',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
